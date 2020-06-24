417937f1c12d67d78fd852337709aecd72ac153f
BenWix <benwichser@gmail.com>
2020-06-24 10:52:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("input[value='Create New Task']").addEventListener("click", function(event) {
     event.preventDefault();
     let task = document.getElementById("new-task-description").value;
-    console.log(task);
     let newTaskElement = document.createElement('li');
     newTaskElement.innerText = task;
     document.getElementById("list").appendChild(newTaskElement);
