1c985f47c15f3d6a34ac4206ec4a12b1821f58a6
ferrisbueller66 <github email address>
2020-06-25 10:15:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let submit = document.getElementsByTagName('input');
+  let tasks = document.getElementById('tasks');
+  submit[1].addEventListener('click', function(event) { 
+      let taskDescription = document.getElementById('new-task-description').value;
+      event.preventDefault();
+      let newListItem = document.createElement('li');
+      tasks.appendChild(newListItem);
+      newListItem.textContent = taskDescription
+    }, 
+    false);
+
+
+
+
 });
+
