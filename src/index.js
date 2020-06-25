14c5bb4b8f10fb71363d9f746505cbf27c398835
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-25 13:52:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const tasksContainer = document.querySelector('#tasks')
+
+  const taskForm = document.getElementById('create-task-form')
+
+  taskForm.addEventListener('submit', () => {
+    event.preventDefault()
+    console.log("The button was clicked!")
+    console.log(event)
+    const userInputField = event.target.querySelector("#new-task-description")
+    const userInput = userInputField.value 
+    const liTag = document.createElement('li')
+    liTag.textContent = userInput
+    tasksContainer.appendChild(liTag)
+    userInputField.value = ""
+  })
 });
