63f45cd61b77f18051d65ded4ae14d770283c7eb
Nashmeyah <nashalrekabi@gmail.com>
2020-06-28 21:22:18 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+let tasks = document.querySelector("#tasks");
+let text = document.querySelector("#new-task-description");
+let form = document.querySelector("#create-task-form");
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", function (e) {
+    let li = document.createElement("li");
+    tasks.appendChild(li);
+
+    li.textContent = text.value;
+    e.preventDefault();
+  });
 });
