29c79433e31939c20c0a44529811c955c302e816
Ryan-McFadden <ryanmcfadden123@gmail.com>
2020-06-28 22:21:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form")
+
+  let textBox =  document.getElementById("new-task-description")
+  
+  let tasks = document.getElementById('tasks')
+  
+  form.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    let li = document.createElement("li")
+
+    li.innerText = textBox.value
+    tasks.append(li)
+  })
 });
