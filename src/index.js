8d2f3b772d66576c77ede3670326635b7251249e
Christopher <reinkendigital@gmail.com>
2020-07-01 22:22:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let ul = document.getElementById("tasks");
+    let text = document.getElementById("new-task-description");
+    document.querySelectorAll('input')[1].addEventListener("click", function(e) {
+        let li = document.createElement('li');
+        li.innerHTML = text.value;
+        ul.appendChild(li);
+
+        e.preventDefault();
+    }, false);
 });
