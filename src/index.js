475c12c3666b2351bc2afdc8fc654e58ed1bfda8
nisticism <nistacular@gmail.com>
2020-07-08 15:45:19 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const add_task = document.querySelectorAll('form input')[1]
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  add_task.addEventListener("click", function (event) {
+
+    displayTask();
+    event.preventDefault();
+  });
+
 });
+
+function displayTask() {
+  let list = document.querySelector('ul#tasks')
+  let task = document.createElement("li");
+  list.appendChild(task);
+  task.textContent = document.querySelectorAll('form input')[0].value;
+}
