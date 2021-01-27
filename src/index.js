31b8701aa419416bbf3169c5b8d04ce3cfe561cf
Beth Ruff <beth.ruff2@gmail.com>
2021-01-27 10:23:00 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addTask);
+})
+
+function addTask(e){
+    e.preventDefault();
+    console.log(e.target.querySelector('#new-task-description').value);
+    let ul = document.getElementById("tasks");
+    let li = document.createElement('li');
+    li.innerText = e.target.querySelector('#new-task-description').value;
+    ul.appendChild(li);
+    e.target.querySelector('#new-task-description').value = "";
+}
